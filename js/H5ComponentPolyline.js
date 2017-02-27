a072762326f604d33179706ca472956a6f202072
Xuyifan91 <seaforest_x@icloud.com>
2017-02-27 18:19:17 +0800

    h5

---
+++ js/H5ComponentPolyline.js
@@ -0,0 +1,113 @@
+/*Canvas折线图组件对象 */
+var H5ComponentPolyline= function(name,cfg){
+    var component = new H5ComponentBase(name,cfg);
+
+    var w = cfg.width;
+    var h = cfg.height;
+    var cns = document.createElement('canvas');
+    var ctx = cns.getContext('2d');
+    cns.width = ctx.width=w;
+    cns.height = ctx.height = h;
+    component.append(cns);
+
+    //绘制背景网格层
+    var step=10;
+    ctx.beginPath();
+    ctx.lineWidth=1;
+    ctx.strokeStyle = '#aaa';
+    window.ctx = ctx;
+    for(var i=0;i<step+1;i++){
+        var y = (h/step)*i;
+        ctx.moveTo(0,y);
+        ctx.lineTo(w,y);
+    }
+
+    step =cfg.data.length+1;
+    var text_w = (w/step)>>0;
+    for(var i=0;i<step+1;i++){
+        var x =( w/step )*i;
+        ctx.moveTo(x,0);
+        ctx.lineTo(x,h);
+        if(cfg.data[i]){
+        var text = $('<div class="text">');
+        text.text(cfg.data[i][0]);
+        text.css('width',text_w/2).css('left',x/2+text_w/4);
+        component.append(text);
+        }
+    }
+    ctx.stroke();
+    //绘制数据层
+    var cns = document.createElement('canvas');
+    var ctx = cns.getContext('2d');
+    cns.width = ctx.width=w;
+    cns.height = ctx.height = h;
+    component.append(cns);
+
+
+    var draw = function(per){
+            ctx.clearRect(0,0,w,h);
+
+            ctx.beginPath();
+            ctx.lineWidth=3;
+            ctx.strokeStyle = '#ff8878';
+            //画点
+            var x = 0;
+            var y = 0;
+            var row_w = (w/(cfg.data.length+1));
+            for(i in cfg.data){
+                var item = cfg.data[i];
+                x = row_w*i+row_w ;
+                y =h-(item[1]*h*per);
+                ctx.moveTo(x,y);
+                ctx.arc(x,y,5,0,2*Math.PI);
+            }
+            //连线
+            ctx.moveTo(row_w,h-(cfg.data[0][1]*h*per));
+        /*    ctx.arc(row_w,h*(1-cfg.data[0][1]),20,0,2*Math.PI);*/
+            for(i in cfg.data){
+                var item = cfg.data[i];
+                x =  row_w*i+row_w;
+                y =h-(item[1]*h*per);
+                ctx.lineTo(x,y);
+            }
+            ctx.stroke();
+            ctx.lineWidth = 1;
+            ctx.strokeStyle ='rgba(255,118,118,0)';
+            //绘制阴影
+            ctx.lineTo(x,h);
+            ctx.lineTo(row_w,h);
+            ctx.fillStyle='rgba(255,118,118,0.23)';
+            ctx.fill();
+            //写数据
+
+            for(i in cfg.data){
+                var item = cfg.data[i];
+                x = row_w*i+row_w ;
+                y =h-(item[1]*h*per);
+                ctx.fillStyle = item[2]?item[2]:'#595959';
+                ctx.fillText(((item[1]*100)>>0)+'%',x-10,y-14);
+            }
+            ctx.stroke();
+    };
+    /*draw(s);*/
+    component.on('onLoad',function(){
+        var s = 0;
+        for(var i=0;i<100;i++){
+            setTimeout(function(){
+                s+=.01;
+                draw(s);
+            },i*10+600)
+        }
+    });
+    component.on('onLeave',function(){
+        var s = 1;
+        for(var i=0;i<100;i++){
+            setTimeout(function(){
+                s-=.01;
+                draw(s);
+            },i*10)
+        }
+    });
+
+    return component
+};
---
+++ js/H5ComponentRadar.js
@@ -0,0 +1,151 @@
+/* 雷达图组件对象 */
+var H5ComponentRadar= function(name,cfg){
+    var component = new H5ComponentBase(name,cfg);
+
+    var w = cfg.width;
+    var h = cfg.height;
+    var cns = document.createElement('canvas');
+    var ctx = cns.getContext('2d');
+    cns.width = ctx.width=w;
+    cns.height = ctx.height = h;
+    component.append(cns);
+
+    var r = w/2;
+    var step = cfg.data.length;
+ /*   ctx.beginPath();
+    ctx.arc(r,r,5,0,2*Math.PI);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(r,r,r,0,2*Math.PI);
+    ctx.stroke();
+*/
+    //多边形顶点坐标公式  已知 圆心坐标a，b 半径r 角度deg
+    //rad = (2*Math.PI/360)*(360/step)*i
+    //x = a+Math.sin(rad)*r;
+    //y= b+Math.cos(rad)*r;
+
+    //绘制网格背景
+    var isBlue = true;
+    for(var s=10;s>0;s--){
+        ctx.beginPath();
+        for(var i=0;i<step;i++){
+            var rad = (2*Math.PI/360)*(360/step)*i;
+            var x = r+Math.sin(rad)*r*(s/10);
+            var y= r+Math.cos(rad)*r*(s/10);
+            //ctx.arc(x,y,5,0,2*Math.PI);
+            //ctx.moveTo(r,r);
+            ctx.lineTo(x,y);
+        }
+        ctx.closePath();
+        ctx.fillStyle=(isBlue = !isBlue)?'#99c0ff' :'#f1f9ff';
+        ctx.fill();
+    }
+    //绘制伞骨
+    for(var i=0;i<step;i++){
+        var rad = (2*Math.PI/360)*(360/step)*i;
+        var x = r+Math.sin(rad)*r;
+        var y= r+Math.cos(rad)*r;
+        ctx.moveTo(r,r);
+        ctx.lineTo(x,y);
+        //项目文字
+        var text = $('<div class="text">');
+        text.text(cfg.data[i][0]);
+        text.css('transition','all .5s '+ i*.15 +'s');
+        if(x>w/2){
+            text.css('left',x/2);
+        }else{
+            text.css('right',(w-x)/2+5);
+        }
+        if(y>h/2){
+            text.css('top',y/2);
+        }else{
+            text.css('bottom',(h-y)/2+5)
+        }
+        if(cfg.data[i]){
+            text.css('color',cfg.data[i][2])
+        }
+        text.css('opacity',0);
+        component.append(text);
+
+    }
+
+    ctx.strokeStyle = '#e0e0e0';
+    ctx.stroke();
+
+  /*  ctx.beginPath();
+    for(var i=0;i<step;i++){
+        var rad = (2*Math.PI/360)*(360/step)*i;
+        var x = r+Math.sin(rad)*r*.5;
+        var y= r+Math.cos(rad)*r*.5;
+        //ctx.arc(x,y,5,0,2*Math.PI);
+        //ctx.moveTo(r,r);
+        ctx.lineTo(x,y);
+    }
+    ctx.fillStyle='#f00';
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();*/
+    var cns = document.createElement('canvas');
+    var ctx = cns.getContext('2d');
+    cns.width = ctx.width=w;
+    cns.height = ctx.height = h;
+    component.append(cns);
+
+    ctx.strokeStyle='#f00';
+
+    var draw = function(per){
+        if(per<=1){
+            component.find('.text').css('opacity',0);
+        }
+        if(per>=1){
+            component.find('.text').css('opacity',1);
+        }
+            ctx.clearRect(0,0,w,h);
+        ctx.fillStyle='rgba(255,118,118,0.08)' ;
+            for(var i=0;i<step;i++){
+                var rad = (2*Math.PI/360)*(360/step)*i;
+                var rate = cfg.data[i][1]*per;
+                var x = r+Math.sin(rad)*r*rate;
+                var y= r+Math.cos(rad)*r*rate;
+                ctx.lineTo(x,y);
+                ctx.fill();
+            }
+        ctx.closePath();
+        ctx.stroke();
+
+        ctx.fillStyle='#ff7676';
+        for(var i=0;i<step;i++){
+            var rad = (2*Math.PI/360)*(360/step)*i;
+            var rate = cfg.data[i][1]*per;
+            var x = r+Math.sin(rad)*r*rate;
+            var y= r+Math.cos(rad)*r*rate;
+            ctx.beginPath();
+            ctx.arc(x,y,5,0,2*Math.PI);
+            ctx.fill();
+            ctx.closePath();
+        }
+
+    };
+    //draw(.5);
+    component.on('onLoad',function(){
+        var s = 0;
+        for(var i=0;i<100;i++){
+            setTimeout(function(){
+                s+=.01;
+              draw(s);
+            },i*10+600)
+        }
+    });
+    component.on('onLeave',function(){
+        var s = 1;
+        for(var i=0;i<100;i++){
+            setTimeout(function(){
+                s-=.01;
+              draw(s);
+            },i*10)
+        }
+    });
+
+    return component
+}
